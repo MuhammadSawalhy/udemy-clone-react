29e532bf8e03beb451078c2ff96f689908535b86
محمد الصوالحي <ms.2052001@gmail.com>
2022-09-08 12:02:55 +0200

    fix material ui icons size due to base 10px font size

--- src/index.js
+++ src/index.js
@@ -7,12 +7,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Routes from "./widgets/Routes";
 import NavigationBar from "./widgets/NavigationBar";
+import { ThemeProvider, createTheme } from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const theme = createTheme({
+  typography: {
+    // to make the icons a little bit bigger
+    // they are small because I added this in the css
+    // ->    :root { font-size: 10px; }
+    fontSize: 20,
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <NavigationBar />
-    <Routes />
+    <ThemeProvider theme={theme}>
+      <NavigationBar />
+      <Routes />
+    </ThemeProvider>
   </React.StrictMode>
 );
