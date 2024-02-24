8f5b02970f1c764b5efd9e9f014269853345fb8b
محمد الصوالحي <muhammadsawalhy@gmail.com>
2024-02-24 07:36:58 +0200

    Upgrade packages 📦

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { ViteAliases } from "vite-aliases";
 
-export default ({ mode }) => {
+export default () => {
   return defineConfig({
     base: '',
     plugins: [react(), ViteAliases({ useConfig: true })],
